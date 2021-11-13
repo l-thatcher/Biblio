aa5f7ab4d02822962d8f16d3b5dba740ac50021f
C1989684 <thatcherlg@cardiff.ac.uk>
2021-11-13 16:14:05 +0000

    Initial commit, set up base framework and a log in/register interface

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class constants {
+  static const regularText = TextStyle( fontSize: 14.0, fontWeight: FontWeight.w600, color: Colors.black);
+
+  static const headingText = TextStyle( fontSize: 22.0, fontWeight: FontWeight.w800, color: Colors.black);
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:biblio_files/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          child: Text("logout"),
+          onPressed: () {
+            FirebaseAuth.instance.signOut();
+          },
+        ),
+      ),
+    );
+  }
+}
