259b1d11917794b71a881686bbd22709c9bf7c3b
C1989684 <thatcherlg@cardiff.ac.uk>
2021-11-20 12:38:50 +0000

    messaging front end work

--- lib/widgets/chat_pages/chat_page.dart
+++ lib/widgets/chat_pages/chat_page.dart
@@ -1,4 +1,3 @@
-import 'package:biblio_files/widgets/custom_image_button.dart';
 import 'package:biblio_files/widgets/custom_input_field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
