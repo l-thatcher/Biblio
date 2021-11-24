b215b32b4860619b79d470eccb078d0e0c562953
C1989684 <thatcherlg@cardiff.ac.uk>
2021-11-24 19:43:48 +0000

    front end verification for images

--- lib/screens/new_post_page.dart
+++ lib/screens/new_post_page.dart
@@ -98,9 +98,13 @@ class _NewPostpageState extends State<NewPostpage> {
   }
 
   Future<String?> _createPost() async {
+    print(uploadImg1.image);
     if (name == null) {
       return "Please give this listing a name to upload it.";
-    } else {
+    } else if (uploadImg1.image == null || uploadImg2.image == null || uploadImg3.image == null || uploadImg4.image == null){
+      return "Please give this listing at least one image to upload it.";
+    }
+    else {
       try {
         if (uploadImg1.image != null){
           image1Url = await (uploadFile(uploadImg1.image));
